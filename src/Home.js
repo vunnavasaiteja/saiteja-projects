7fbdf7c78aa341f4d86a9d97b23a3d2101653d8b
vunnavasaiteja <vunnavasaiteja@gmail.com>
2023-08-09 17:38:46 +0530

    adding amazon-clone

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PC/MED/CE/MED_Aug23_OC_tallhero_3000x1200._CB597700369_.jpg" alt=''/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyASmICVg9U_eex-dCfPbMh8X3UVeg4PkD4",
+    authDomain: "challenge-1e9bc.firebaseapp.com",
+    projectId: "challenge-1e9bc",
+    storageBucket: "challenge-1e9bc.appspot.com",
+    messagingSenderId: "427381645787",
+    appId: "1:427381645787:web:a05da5baa3a0ac8fd187fe",
+    measurementId: "G-G6YYNLH6QH"
+  };
